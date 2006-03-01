445e32f15219a6de45732533577b16adb5004ce0
David Gibson <dgibson@sneetch.ozlabs.ibm.com>
2006-03-01 11:41:58 +1100

    Make the chunk-overcommit test more selective.
    This patch improves the chunk-overcommit test.  Previously it would assume
    any apparent overcommit was bad, and FAIL accordingly.  It now goes on to test
    if the overcommit actually results in a SIGKILL (or other failure) when
    you attempt to instiate all the allocated pages.  This means this test will
    not give spurious failures if we ever get a kernel which automatically tries
    to make more hugepages, or can swap hugepages or can otherwise cope with an
    overcommit situation.
    
    Also added a test rationale comment.

--- tests/chunk-overcommit.c
+++ tests/chunk-overcommit.c
@@ -1,4 +1,18 @@
 /*
+ * Test rationale:
+ *
+ * Some kernel versions after hugepage demand allocation was added
+ * used a dubious heuristic to check if there was enough hugepage
+ * space available for a given mapping.  The number of
+ * not-already-instantiated pages in the mapping was compared against
+ * the total hugepage free pool.  It was very easy to confuse this
+ * heuristic into overcommitting by allocating hugepage memory in
+ * chunks, each less than the total available pool size but together
+ * more than available.  This would generally lead to OOM SIGKILLs of
+ * one process or another when it tried to instantiate pages beyond
+ * the available pool.
+ *
+ *
  * libhugetlbfs - Easy use of Linux hugepages
  * Copyright (C) 2005-2006 David Gibson & Adam Litke, IBM Corporation.
  *
@@ -17,12 +31,16 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
+#include <signal.h>
 
 #include <hugetlbfs.h>
 
@@ -36,6 +54,8 @@ int main(int argc, char *argv[])
 	unsigned long totpages, chunk1, chunk2;
 	int fd;
 	void *p, *q;
+	pid_t child, ret;
+	int status;
 
 	test_init(argc, argv);
 
@@ -72,6 +92,30 @@ int main(int argc, char *argv[])
 			PASS();
 	}
 
-	FAIL("Overcommitted");
+	verbose_printf("Looks like we've overcommitted, testing...\n");
+
+	/* Looks like we're overcommited, but we need to confirm that
+	 * this is bad.  We touch it all in a child process because an
+	 * overcommit will generally lead to a SIGKILL which we can't
+	 * handle, of course. */
+	child = fork();
+	if (child < 0)
+		FAIL("fork(): %s", strerror(errno));
+
+	if (child == 0) {
+		memset(p, 0, chunk1*hpage_size);
+		memset(q, 0, chunk2*hpage_size);
+		exit(0);
+	}
+
+	ret = waitpid(child, &status, 0);
+	if (ret < 0)
+		FAIL("waitpid(): %s", strerror(errno));
+
+	if (WIFSIGNALED(status))
+		FAIL("Killed by signal \"%s\" due to overcommit",
+		     strsignal(WTERMSIG(status)));
+
+	PASS();
 }
 
