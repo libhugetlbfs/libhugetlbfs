e0759c3dd5ab4f358e5f603d59ec67430dee5c72
Eric B Munson <ebmunson@us.ibm.com>
2009-12-15 18:47:41 +0000

    Add ability to monitor multiple events with oprofile
    This patch adds the ability for the oprofile collector to monitor
    multiple event counters.  This will be useful in computing
    estimates of percentage of CPU cycles spent servicing TLB misses,
    ratios of TLB misses to CPU cycles spent or instructions retired, etc.
    
    Signed-off-by: Eric B Munson <ebmunson@us.ibm.com>

--- TLBC/OpCollect.pm
+++ TLBC/OpCollect.pm
@@ -18,8 +18,9 @@ use TLBC::DataCollect;
 
 our @ISA = qw(TLBC::DataCollect);
 
-my $count = 0;
 my $reference;
+my (%event_map, %lowlevel);
+my (%event_col_map, %event_name);
 
 #use interface 'DataCollect';
 
@@ -33,31 +34,33 @@ sub _clear_oprofile()
 	return $self;
 }
 
-sub _setup_oprofile()
-{
-	my $self = shift;
-	my $vmlinux = shift;
-	my $event = shift;
-	my $cmd = "$Bin/oprofile_start.sh --vmlinux=$vmlinux " .
-		   "--event=$event  > /dev/null 2>&1";
-	system($cmd) == 0 or return 0;
-	return $self;
-}
-
 sub _get_event()
 {
 	my $self = shift;
 	my $event = shift;
-	my @vals;
 	my $lowlevel_event;
+
 	$lowlevel_event = `$Bin/oprofile_map_events.pl --event $event 2>/dev/null`;
 	chomp($lowlevel_event);
 	if ($lowlevel_event eq "" || $lowlevel_event !~ /^[A-Z0-9_]+:[0-9]+/) {
 		die "Unable to find $event event for this CPU\n";
 	}
+	$event_map{$event} = $lowlevel_event;
+	return $self;
+}
 
-	@vals = split(/:/, $lowlevel_event);
-	$count = $vals[1];
+sub _setup_oprofile()
+{
+	my $self = shift;
+	my $vmlinux = shift;
+	my $refEvents = shift;
+	my $cmd = "$Bin/oprofile_start.sh --vmlinux=$vmlinux ";
+	foreach my $event (@{$refEvents}) {
+		$cmd .= " --event=$event";
+		$self->_get_event($event);
+	}
+	$cmd .= " > /dev/null 2>&1";
+	system($cmd) == 0 or return 0;
 	return $self;
 }
 
@@ -77,15 +80,55 @@ sub setup()
 {
 	my $self = shift;
 	my $vmlinux = shift;
-	my $event = shift;
-	$self->_get_event($event);
+	my $refEvents = shift;
 	$self->_clear_oprofile();
-	return $self->_setup_oprofile($vmlinux, $event);
+	return $self->_setup_oprofile($vmlinux, $refEvents);
 }
 
 sub samples()
 {
-	return $count;
+	my $self = shift;
+	my $event = shift;
+	my $count = 0;
+	my $lowlevel;
+	my @vals;
+	$lowlevel = $event_map{$event};
+	if (!$lowlevel) {
+		die "Unable to locate count and event for $event for this CPU.\n";
+	}
+	@vals = split(/:/, $lowlevel);
+	return $vals[1];
+}
+
+sub _get_column()
+{
+	my $self = shift;
+	my $report = shift;
+	my $event = shift;
+	my @results;
+	my $line;
+	my $col = $event_col_map{$event};
+
+	if ($col) {
+		return $col;
+	}
+
+	@results = split(/\n/, $report);
+	foreach $line (@results) {
+		if ($line =~ /$event.*\|/) {
+			my @vals = split(/\|/, $line);
+			my $size = @vals;
+
+			for (my $i = 0; $i < $size; $i++) {
+				if ($vals[$i] =~ /$event/) {
+					$event_col_map{$event} = $i;
+					return $i;
+				}
+			}
+			die "Unable to find event column.\n";
+		}
+	}
+	die "Unable to find column labels.\n";
 }
 
 sub get_current_eventcount()
@@ -96,17 +139,31 @@ sub get_current_eventcount()
 	my $hits = 0;
 	my $self = shift;
 	my $binName = shift;
+	my $event = shift;
+	my $col = 0;
 
+	my $lowlevel = $event_map{$event};
+	if (!$lowlevel) {
+		die "Unable to locate event for $event for this CPU.\n";
+	}
+	my @vals = split(/:/, $lowlevel);
+	$event = $vals[0];
+	# The event column in opreport only uses the first 12 letters of
+	# the event name
+	$event = substr($event, 0, 12);
 	system("opcontrol --dump > /dev/null 2>&1");
 	$report = `opreport`;
 	@results = split(/\n/, $report);
+	$col = $self->_get_column($report, $event);
+
 	foreach $line (@results) {
 		if ($line =~ /$binName/) {
 			chomp($line);
 			$line =~ s/^\s+//;
 			$line =~ s/\s+$//;
+			$line =~ s/\s+/ /g;
 			my @vals = split(/ /, $line);
-			$hits += $vals[0];
+			$hits += $vals[$col * 2];
 		}
 	}
 	return $hits;
