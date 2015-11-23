77691247a188fa69ead398eb37d3c2d38503ef1b
David Gibson <david@gibson.dropbear.id.au>
2015-11-23 15:31:20 +1100

    Fix "set but not used" warnings
    It seems libhugetlbfs was never audited for the "set but not used" warning
    added to gcc a while back.  This fixes a number of instances of such
    warnings.
    
    Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

--- tests/icache-hygiene.c
+++ tests/icache-hygiene.c
@@ -154,13 +154,14 @@ static void sig_handler(int signum, siginfo_t *si, void *uc)
 static void test_once(int fd)
 {
 	void *p, *q;
-	int dummy;
 
-	dummy = ftruncate(fd, 0);
+	if (ftruncate(fd, 0) < 0)
+		FAIL("ftruncate() 1: %s\n", strerror(errno));
 
 	if (sigsetjmp(sig_escape, 1)) {
 		sig_expected = NULL;
-		dummy = ftruncate(fd, 0);
+		if (ftruncate(fd, 0) < 0)
+			FAIL("ftruncate() 2: %s\n", strerror(errno));
 		return;
 	}
 
@@ -169,13 +170,15 @@ static void test_once(int fd)
 	if (p == MAP_FAILED)
 		FAIL("mmap() 1: %s", strerror(errno));
 
-	dummy = ftruncate(fd, hpage_size);
+	if (ftruncate(fd, hpage_size) < 0)
+		FAIL("ftruncate() 3: %s\n", strerror(errno));
 
 	q = p + hpage_size - COPY_SIZE;
 
 	jumpfunc(1, q);
 
-	dummy = ftruncate(fd, 0);
+	if (ftruncate(fd, 0) < 0)
+		FAIL("ftruncate() 4: %s\n", strerror(errno));
 	p = mmap(p, hpage_size, PROT_READ|PROT_WRITE|PROT_EXEC,
 		 MAP_SHARED|MAP_FIXED, fd, 0);
 	if (p == MAP_FAILED)
